e7f563b7cbbc2859f2a583f99ab74525777eddfe
antonis <denti20082008@hotmail.com>
2018-06-30 20:27:30 +0300

    UI tweaks and now progress bar is shown

--- cityRaise-frontend/src/app/publicWorks.component.ts
+++ cityRaise-frontend/src/app/publicWorks.component.ts
@@ -17,14 +17,37 @@ export class PublicWorksComponent implements OnInit {
   constructor(private webService: WebService/*, private authService: AuthService*/) {
   }
 
+  i = 0;
+
   async ngOnInit() {
     // if (sessionStorage.start) {
     //   this.start = sessionStorage.start;
     // }
+    this.i = 0;
 
     console.log('HELLO');
     this.webService.getPublicWorks();
     // this.webService.getCarsCount();
     // this.getKeyDown();
   }
+
+  showPercentage(c_funds, r_funds) {
+    console.log('current funds: ' + (Number(c_funds[0]) + Number(c_funds[1])) + ' ' + 'required_funds: ' + r_funds);
+    const elements = document.getElementsByClassName('progress-bar') as HTMLCollectionOf<HTMLElement>;
+    const width = ((Number(c_funds[0]) + Number(c_funds[1])) / r_funds) * 100;
+    if (elements[this.i] !== undefined) {
+      elements[this.i].setAttribute('style', 'width: ' + width + '%');
+    }
+    console.log(elements[this.i]);
+    this.i++;
+
+    // $( ".progress-bar" ).each(function(index) {
+    //   console.log( index + ": " + percentage[index] );
+    //   $( this ).width(percentage[index] + "%");
+    // });
+  }
+
+  // getPercentage(c_funds, r_funds) {
+  //   return ((Number(c_funds[0]) + Number(c_funds[1])) / r_funds) * 100;
+  // }
 }
